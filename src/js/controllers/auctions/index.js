f501abe2f7261f8eee84a9890703ce95bac3eff8
delamorinierejh <delamorinierejh@gmail.com>
2016-10-27 09:25:14 +0100

    pushing minor change to index page

--- src/js/controllers/auctions/index.js
+++ src/js/controllers/auctions/index.js
@@ -19,23 +19,31 @@ function AuctionsIndexCtrl(Auction, CurrentUserService){
       return b.reserve - a.reserve;
     }).reverse();
     newIndex();
+    $("ul.sortbuttons li a").css({"background-color": "transparent", "color" : "#225560"});
+    $("#button1").css({"background-color": "#225560", "color" : "white"});
   };
   vm.sortByReserveDesc = ()=> {
     vm.auctions.sort(function(a,b){
       return b.reserve - a.reserve;
     });
     newIndex();
+    $("ul.sortbuttons li a").css({"background-color": "transparent", "color" : "#225560"});
+    $("#button2").css({"background-color": "#225560", "color" : "white"});
   };
   vm.sortByExpirySoonest = ()=> {
     vm.auctions.sort(function(a,b){
       return new Date(b.end_date) - new Date(a.end_date);
     }).reverse();
     newIndex();
+    $("ul.sortbuttons li a").css({"background-color": "transparent", "color" : "#225560"});
+    $("#button3").css({"background-color": "#225560", "color" : "white"});
   };
   vm.sortByExpiryFurthest = ()=> {
     vm.auctions.sort(function(a,b){
       return new Date(b.end_date) - new Date(a.end_date);
     });
     newIndex();
+    $("ul.sortbuttons li a").css({"background-color": "transparent", "color" : "#225560"});
+    $("#button4").css({"background-color": "#225560", "color" : "white"});
   };
 }
