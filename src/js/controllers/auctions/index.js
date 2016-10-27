11ac28a6e471de083996b17b6196f2bb64b448cf
delamorinierejh <delamorinierejh@gmail.com>
2016-10-27 01:05:59 +0100

    updated index auctions layout and profile stuff too

--- src/js/controllers/auctions/index.js
+++ src/js/controllers/auctions/index.js
@@ -9,27 +9,33 @@ function AuctionsIndexCtrl(Auction, CurrentUserService){
   .query()
   .$promise
   .then(data => {
-    console.log(data);
     vm.auctions = data;
   });
+  function newIndex(){
+    $("#auctionslist").hide().fadeIn(400);
+  }
   vm.sortByReserveAsc = ()=> {
     vm.auctions.sort(function(a,b){
       return b.reserve - a.reserve;
     }).reverse();
+    newIndex();
   };
   vm.sortByReserveDesc = ()=> {
     vm.auctions.sort(function(a,b){
       return b.reserve - a.reserve;
     });
+    newIndex();
   };
   vm.sortByExpirySoonest = ()=> {
     vm.auctions.sort(function(a,b){
       return new Date(b.end_date) - new Date(a.end_date);
     }).reverse();
+    newIndex();
   };
   vm.sortByExpiryFurthest = ()=> {
     vm.auctions.sort(function(a,b){
       return new Date(b.end_date) - new Date(a.end_date);
     });
+    newIndex();
   };
 }
